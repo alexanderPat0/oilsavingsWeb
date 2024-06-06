acd42946bf4b708e5dc69636f51e33f256a76fdb
alexanderPat0 <alex0rodriguez7fenix@gmail.com>
2024-06-06 17:37:52 +0200

    Importo archivos para breeze

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
-// web.php
-
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('users', [UserController::class, 'indexUsers'])->name('users.index');
-Route::get('admins', [UserController::class, 'indexAdmins'])->name('admins.index');
-Route::get('users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('users', [UserController::class, 'store'])->name('users.store');
-Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
