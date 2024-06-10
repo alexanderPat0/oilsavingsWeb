c1b3df849e8436949f7883be2a41768ddefa19b2
alexanderPat0 <alex0rodriguez7fenix@gmail.com>
2024-06-11 00:07:53 +0200

    eliminado el app.lsiten /

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
-});
-
 app.listen(PORT, () => {
   console.log(`Aplicación ejecutándose en el puerto ${PORT}`);
 });
