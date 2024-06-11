2871e1d6fe67fa767bb7756b489c05f9710f3515
alex0rodriguez7fenix <alex0rodriguez7fenix@gmail.com>
2024-06-11 08:10:52 +0000

    añadiddo el app.get /

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hola Mundo!');
+});
+
 app.listen(PORT, () => {
   console.log(`Aplicación ejecutándose en el puerto ${PORT}`);
 });
