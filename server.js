ee04ae2a4bb03b2f6ae57224b7c23194ec54e3f8
alex0rodriguez7fenix <alex0rodriguez7fenix@gmail.com>
2024-06-11 09:06:27 +0000

    res-send200Ok

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
+  res.status(200).send('OK');
 });
 
 app.listen(PORT, () => {
